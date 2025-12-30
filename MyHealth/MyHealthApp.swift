724754d576f5280381fb7bf56069eb27904e14e3
berkahhariini999-rgb <berkahhariini999@gmail.com>
2025-12-30 13:41:36 +0700

    Initial Commit

---
+++ MyHealth/MyHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHealthApp.swift
+//  MyHealth
+//
+//  Created by Iqbal Alhadad on 30/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MyHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
